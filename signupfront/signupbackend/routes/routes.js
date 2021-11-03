0c2cdbfbdf786d53152221489cee2a0ada581ce2
cosy5104 <cosmaskip365@gmail.com>
2021-11-04 02:49:00 +0300

    changes added

---
+++ signupfront/signupbackend/routes/routes.js
@@ -0,0 +1,26 @@
+const express= require('express')
+const router = express.Router()
+const signUpTemplateCopy=require('../models/Signupmodels.js')
+const bcrypt=require('bcrypt')
+
+router.post('/signup',async(req,res)=>{
+
+    const saltPassword=await bcrypt.genSalt(10)
+    const securePassword=await bcrypt.hash(req.body.password,saltPassword)
+    const signedUpUser=new signUpTemplateCopy({
+        fullName:req.body.fullName,
+        username:req.body.username,
+        email:req.body.email,
+        password:securePassword
+    })
+    //res.send("send")
+    signedUpUser.save()
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(error=>{
+        res.json(error)
+    })
+})
+
+module.exports=router
